1c536492380fadd54e12cbcfb0d78042f0713973
Jia-Jia Zhu <dodge_waking_0b@icloud.com>
2023-07-31 23:02:48 -0700

    lab complete

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(blocks) {
+  return Math.abs(42 - blocks);
+}
+
+function distanceFromHqInFeet(blocks) {
+  return Math.abs(264 * (42 - blocks));
+}
+
+function distanceTravelledInFeet(start, end) {
+  return 264 * Math.abs(start - end);
+}
+
+function calculatesFarePrice(start, end) {
+  if (264 * Math.abs(start - end) <= 400) {
+    return 0;
+  } else if (
+    264 * Math.abs(start - end) > 400 &&
+    264 * Math.abs(start - end) <= 2000
+  ) {
+    return Math.abs(0.02 * (400 - 264 * Math.abs(start - end)));
+  } else if (
+    264 * Math.abs(start - end) > 2000 &&
+    264 * Math.abs(start - end) <= 2500
+  ) {
+    return 25;
+  } else if (264 * Math.abs(start - end) > 2500) {
+    return "cannot travel that far";
+  }
+}
